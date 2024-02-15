684601127a931e79f7e280d5465a84ffd358a153
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-02-15 16:02:28 +0100

    update

--- OPERATORSANDEQUALITY/logicAndControlFlow/script.js
+++ OPERATORSANDEQUALITY/logicAndControlFlow/script.js
@@ -146,31 +146,7 @@
 
 
 
-//SWITCH-STATEMENT
-   
-  //Switch-Statement: used to perform different operations and it;s similar to if & else.
-  //comprise of Switch keywords, such as; -case, -Break & Default.
-  //Break:-
 
-//   let superHero = "Captain America";
-
-//   switch (superHero) {
-//     case 'iron Man':
-//       console.log("i am iron Man..")
-//       break;
-//       case 'Thor':
-//         console.log("That is my hammar!")
-//         break;
-//         case 'Captain America' :
-//           console.log("Never give up!")
-//           break;
-//           case 'Black Widow':
-//             console.log('One short, One kill.!')
-//             break;
-//             default:
-//               console.log("Enter a valid superHero Name")
-
-//  }
 
 
   // console.log(superHero)
@@ -210,44 +186,9 @@
 
 
   
-// let text = (new Date().getDay()) 
-// Switch (day){
-//   case 6:
-//     text = "Today is Saturday";
-//     break;
-//   case 0:
-//     text = "Today is Sunday";
-//     break;
-//   default:
-//     text = "Looking forward to the Weekend";
-// }
-// console.log(text)
 
 
-let FruitName = "Apple"
-let message;
 
-switch (FruitName) {
- case 'Mango':
-   message = "sweetend"
-   break;
-
-   case 'Orange':
-   message = "unsweetend"
-   break;
-
-   case 'Apple':
-     message = " Apple are safe and sweet!!"
-     break;
-
-     case 'Banana':
-     message = "unsafe"
-     break;
-
-     default:
-       message = "none of the above"
-}
-console.log(message)
 
 // let fruits = "Banana"
 
