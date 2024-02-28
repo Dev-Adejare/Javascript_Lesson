a3dd71228534de66128f15a9a6b831b4615462e7
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-02-28 12:53:06 +0100

    update

---
+++ DOM_ELEMENT/script.js
@@ -0,0 +1,21 @@
+const tagEl = document.getElementsByTagName('h1');
+console.log(tagEl);
+
+const testEl = document.getElementsByClassName("testing");
+console.log(testEl);
+
+const checkId = document.getElementById("boxText");
+
+const allEl = document.querySelectorAll(".selectEl");
+
+const OneEl = document.querySelector(".selectOne");
+
+const ell = document.querySelector("#pele");
+
+const submitBtn = document.querySelector('button');
+
+console.log(submitBtn);
+
+submitBtn.addEventListener('click', ()=> {
+    alert('Its closing time already')
+})
