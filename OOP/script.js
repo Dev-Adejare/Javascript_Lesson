24c6ee725a73595438f63db5533eb7a78f821367
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-02-29 10:33:21 +0100

    update

--- OOP/script.js
+++ OOP/script.js
@@ -12,3 +12,43 @@ const length = 5;
 const width = 10;
 const area = calculateRectangleArea(length, width);
 console.log("The area of the rectangle is: " + area);
+
+
+//Object_Literal
+
+let person = {
+    first_name : "Salman",
+    last_name : "Kofoshi",
+
+    getFunction : function() {//method ---> a property value is a function
+    return `My name is ${person.first_name} ${person.last_name}`
+    },
+
+    phoneNumber : {
+        mobile: "12345",
+        landline: "6789"
+    }
+}
+console.log(person.getFunction());
+console.log(person.phoneNumber.landline);
+
+//Object_Constructor
+
+function personTwo(first_name, last_name) {
+    this.first_name = first_name,
+    this.last_name = last_name
+}
+
+let person1 = new personTwo("Hayzed", "Mabululu");
+console.log(person1);
+
+//The New Keyword
+
+const student = {
+
+} //simple object
+
+const studentTwo = Object();
+studentTwo.firstName = "Eef"
+studentTwo.lastName = "Eefy"
+console.log(studentTwo);
