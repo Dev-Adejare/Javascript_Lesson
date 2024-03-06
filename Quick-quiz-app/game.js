a595bb11da5e0430ced6290bec76a5ceca2a95ad
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-06 12:26:32 +0100

    update

--- Quick-quiz-app/game.js
+++ Quick-quiz-app/game.js
@@ -1,7 +1,6 @@
 // Get Elements with DOM ----> Create an array of Questions.
 const question = document.getElementById('question');
 const progressText = document.getElementById ('progressText');
-//score, progressBarFull, game, choice-text
 const scoreText = document.getElementsByClassName('score');
 const progressBarFull = document.getElementById('progressBarFull');
 const game = document.getElementById('game');
@@ -11,8 +10,53 @@ let currentQuestion = {};
 let acceptingAnswer = false;
 let score = 0;
 let currentCounter = 0;
-let availavbleQuestion = [];
+let availableQuestion = [];
 
-let Questions = [
-    
+let questions = [
+    {
+        "question": "Inside which HTML element do we put the JavaScript?",
+        "choice1": "<script>",
+        "choice2": "<javascript>",
+        "choice3": "<js>",
+        "choice4": "<scripting>",
+        "answer": 1
+      },
+      {
+        "question": "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        "choice1": "<script href='xxx.js'>",
+        "choice2": "<script name='xxx.js'>",
+        "choice3": "<script src='xxx.js'>",
+        "choice4": "<script file='xxx.js'>",
+        "answer": 3
+      },
+      {
+        "question": " How do you write 'Hello World' in an alert box?",
+        "choice1": "msgBox('Hello World');",
+        "choice2": "alertBox('Hello World');",
+        "choice3": "msg('Hello World');",
+        "choice4": "alert('Hello World');",
+        "answer": 4
+      }
 ]
+
+const CORRECT_BONUS =10;
+const MAX_QUESTIONS = 3;
+
+// Start Quiz
+
+const startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestion = [...questions];
+    console.log(availableQuestion);
+    getNewQuestion()
+};
+
+const getNewQuestion = () => {
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+
+}
+
+startGame();
