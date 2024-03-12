399096bc99775983e228b90b746ab0ca2a4a0fdd
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-12 15:33:46 +0100

    update

--- RANDOM-QUOTE-GEN/main.js
+++ RANDOM-QUOTE-GEN/main.js
@@ -8,3 +8,32 @@ const twitterBtn = document.getElementById('twitter');
 
 const newQuoteBtn = document.getElementById('new-quote');
 
+//https://type.fit/api/quotes
+let apiQuote = [];
+function newQuote() {
+
+    const quote = apiQuote[Math.floor(Math.random() * apiQuote.length)];
+    console.log(quote);
+
+    authorName.textContent = quote.author
+    quoteText.textContent = quote.text
+
+}
+
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuote = await response.json()
+        console.log(apiQuote)
+
+        newQuote()
+    }catch(error) {
+        console.log(error)
+
+    }
+}
+
+getQuotes()
+
