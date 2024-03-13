a4ab9524163cccbe9387bdae5aff0c0d0aa92f1d
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-13 10:30:01 +0100

    update

---
+++ RANDOM-QUOTE-GEN/script.js
@@ -0,0 +1,13 @@
+const quoteCon = document.getElementById('quote-container');
+
+const quoteText = document.getElementById('quote');
+
+const authorName = document.getElementById('author');
+
+const twitterBtn = document.getElementById('twitter');
+
+const newQuoteBtn = document.getElementById('new-quote');
+
+const loader = document.getElementById('loader');
+
+//Get Quote API
