c573267f67eccc0db2fe03c7b8df230d8f458879
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-03-13 10:50:02 +0100

    update

--- RANDOM-QUOTE-GEN/script.js
+++ RANDOM-QUOTE-GEN/script.js
@@ -11,3 +11,23 @@ const newQuoteBtn = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
 //Get Quote API
+
+
+//https://api.quotable.io/random
+
+const getQuote = async() => {
+    const apiUrl = 'https://api.quotable.io/random';
+
+    try {
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        console.log(data)
+
+        authorName.innerText =  data.author;
+        quoteText.innerText = data.content;
+    }catch(error) {
+        console.log('Error fetching quote', error)
+    }
+    
+}
+getQuote()
